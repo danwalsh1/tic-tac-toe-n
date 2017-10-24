49232d938779ae457dbdf3c5b3791d098bfbc65d
danwalsh1 <33032585+danwalsh1@users.noreply.github.com>
2017-10-24 23:48:18 +0100

    Added Print Board Functions
    I updated the client.py file to include functions that will allow the program to print the game board based upon the input board size and list of strings, which I am calling the list of "labels". I also added a function to allow the validation of users entered positions.

--- client.py
+++ client.py
@@ -9,6 +9,75 @@ import socket as s
 import threading as t
 import pickle as p
 
+########################
+### GLOBAL VARIABLES ###
+########################
+
+playerNum = -1
+currTurn = -1
+
+#################
+### FUNCTIONS ###
+#################
+
+def printBlankLine(num):
+    ''' Prints board lines where no label is needed '''
+    print("|".join(['   ' for x in range(num)]))
+
+def printLabelLine(labels):
+    ''' Prints board lnes where labels are needed '''
+    print(" " + " | ".join(labels))
+
+def printLabelController(size, currCount, labels):
+    ''' Prepares input list for one row of the board depending upon count '''
+    toPrint = [""] * size
+    num = currCount*size
+    count = 0
+    while(count < size):
+        toPrint[count] = labels[num]
+        count += 1
+        num += 1
+
+    printLabelLine(toPrint)
+
+def printBorderLine(num):
+    ''' Prints board lines where a horizontal boarder line is needed '''
+    count = 0
+    strLine = ""
+    while(count < num):
+        strLine = strLine + "---"
+        if(count+1 != num):
+            strLine = strLine + "-"
+        count += 1
+    print(strLine)
+
+def printBoard(boardSize, labels):
+    ''' Uses other functions to print the board which has it's size determined by boardSize '''
+    count = 0
+    while(count < boardSize):
+        printBlankLine(boardSize)
+        printLabelController(boardSize, count, labels)
+        printBlankLine(boardSize)
+        if(count < boardSize - 1):
+            printBorderLine(boardSize)
+
+        count += 1
+
+def validatePos(pos, size, labels):
+    ''' Checks whether the given position is a valid position within the given list '''
+    if(pos.isdigit()):
+        #The string is a valid digit
+        pos = int(pos)
+        if(pos >= 0 and pos < size**2):
+            if(labels[pos] != "X" and labels[pos] != "O"):
+                return True
+            else:
+                return False
+        else:
+            return False
+    else:
+        return False
+
 ############
 ### GAME ###
 ############
