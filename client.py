9179b1384fa13a98962630109c857d964284ff51
Daniel Walsh <33032585+danwalsh1@users.noreply.github.com>
2017-11-13 12:43:22 +0000

    Updated Client.py
    Added disclaimer at top of the file

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 #>> Client.py
 #>> Daniel Walsh
+#>> Code not to be used without written permission from Daniel Walsh.
+#>> Credit must be given to Daniel Walsh if code is used once permission is given.
 
 ###############
 ### IMPORTS ###
